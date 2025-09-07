8881e25d2cf7689c4c20a784d7ca0cce5d460eb5
佐藤龍真 <satouryouma@satoushingonoMacBook-puro.local>
2025-09-07 13:39:36 +0900

    new2

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -13,6 +13,7 @@ const Profile = () => {
 こうした旅を通して触れた多様な文化や表現に刺激を受け、Webデザインにも興味を持つようになり、現在はWebデザイナーとしての活動をスタートしています。
 多様なデザインやアートに関心があり、企業の広告やホームページ制作から、個人のWebサイト制作まで、幅広い分野に積極的に関わっていきたいと考えています。
       </div>
+    <div className='interest'>
       <div className='myfav'>Interest</div>
       <div className='myfav-in'>
        <div className='food'>・グルメ</div>
@@ -22,6 +23,7 @@ const Profile = () => {
        <div className='film'>・映画</div>
         <div className='film-in'>DVDを買い集めるほど好きで、上映会にも行きます。最近、飛行機で見たMichel Gondryの「Eternal Sunshine of the Spotless Mind」はどんな方にも面白いと思います。</div>
       </div>
+    </div>
     </section>
   )
 }
